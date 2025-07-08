47b81311a5da615ec91a8ba54836e34f7d1e3f01
Damien <146466854+Youdanshow@users.noreply.github.com>
2025-07-08 21:34:32 +0200

    Set text and button colors to black

--- app/src/main/java/com/example/dsmusic/ui/theme/Color.kt
+++ app/src/main/java/com/example/dsmusic/ui/theme/Color.kt
@@ -4,4 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val PinkAccent = Color(0xFFFF007A)
 val BackgroundBlack = Color(0xFFD3D3D3)
-val TextWhite = Color(0xFFFFFFFF)
+val TextWhite = Color(0xFF000000)
