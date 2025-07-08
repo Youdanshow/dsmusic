cef44a42e876169158696b947a0d96a485d1ce98
Damien <146466854+Youdanshow@users.noreply.github.com>
2025-07-08 15:06:47 +0200

    Fix album search list

--- app/src/main/java/com/example/dsmusic/MainActivity.kt
+++ app/src/main/java/com/example/dsmusic/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
