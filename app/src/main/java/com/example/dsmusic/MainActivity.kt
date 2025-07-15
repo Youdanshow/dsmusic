267efd3bdeeedd31a1fc42525eedcfd2f0e77ed6
Damien <146466854+Youdanshow@users.noreply.github.com>
2025-07-15 15:30:44 +0200

    Fix missing import for stringResource

--- app/src/main/java/com/example/dsmusic/MainActivity.kt
+++ app/src/main/java/com/example/dsmusic/MainActivity.kt
@@ -52,6 +52,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.blur
